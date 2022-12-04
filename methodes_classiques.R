5c87e4ec2804eb041e0fa758c38eec7d929962ab
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2022-12-04 16:50:31 +0100

    Add files via upload

--- methodes_classiques.R
+++ methodes_classiques.R
@@ -41,47 +41,51 @@ input_reg <- function(df, col_na, col_reg){
 
 # On prend le jeu de données des vins de Porto <3
 porto <- na.omit(read.csv("porto.csv")) # na.omit pour le fun
+# On enlève les variables de type et de qualité
+quality <- porto$quality
+porto <- porto[c(-1, -13)]
 # On transforme en facteur le type
-type <- as.factor(porto$type)
+#type <- as.factor(porto$type)
 # On le retire temporairement pour faciliter la tâche
-porto <- porto[-1]
 
+
+# On génère un tableau MCAR
+porto_MCAR<- MCAR(porto, p = 0.4)
+# On remet des colonnes sans NA, pour la méthode de régression
+porto_MCAR$pH <- porto$alcohol
+porto_MCAR$citric.acid <- porto$citric.acid
+porto_MCAR$volatile.acidity<- porto$volatile.acidity
+# Méthode de la moyenne
+porto_MCAR.moy <- input_moy(porto_MCAR)
 
 # Méthode de régression
-# On génère un tableau MCAR
-porto_MCAR.reg<- MCAR(porto, p = 0.4)
-# La fonction MCAR génère des NA pour le tableau entier
-# Or les méthodes de régression utilisent des colonnes sans NA
-# On en rajoute donc
-porto_MCAR.reg[c(3,5,9)] <- porto[c(3,5,9)]
-# Exemple où les colonnes à NA sont imputées via un lm sur les colonnes 3, 5 et 9
-porto_reg <- input_reg(porto_MCAR.reg, (1:12)[-c(3,5,9)] , c(3,5,9))
+# Exemple où les colonnes à NA sont imputées via un lm sur la colonne 
+porto_MCAR.reg <- input_reg(porto_MCAR, (1:10)[-c(2, 3, 9)], c(2, 3, 9))
 
-
+# On remet la quality
+porto$quality <- quality
+porto_MCAR.moy$quality <- quality
+porto_MCAR.reg$quality <- quality
 # On fait des LDA
 library(MASS)
 
-# On remet le type
-porto$type <- type
-porto_reg$type <- type
-
-
 # LDA sur le df originel
-LDA <- lda(porto[,-13], porto$type)
-porto.type <- predict(LDA)$class
-lda_ <- table(porto.type, porto$type)
+LDA <- lda(porto$quality ~ porto$residual.sugar+porto$alcohol , data = porto)
+porto.quality <- predict(LDA)$class
+lda_ <- table(porto.quality, porto$quality)
+
+
+#LDA sur le df avec imputation par moyenne
+LDA_moy <- lda(porto$quality ~ residual.sugar+porto$alcohol, data = porto_MCAR.moy)
+porto.quality_moy <- predict(LDA_moy)$class
+lda_moy <- table(porto.quality_moy, porto_MCAR.moy$quality)
+
 
 # LDA sur le df avec imputation par régression
-LDA2 <- lda(porto_reg[,-13], porto_reg$type)
-porto_reg <- predict(LDA2)$class
-lda_2 <- table(porto_reg, porto$type)
+LDA_reg <- lda(porto$quality ~ residual.sugar+porto$alcohol, data = porto_MCAR.reg)
+porto.quality_reg <- predict(LDA_reg)$class
+lda_reg <- table(porto.quality_reg, porto_MCAR.reg$quality)
 
 
-# C'est pas si mauvais, ...
-diag(prop.table(lda_, 1))
-diag(prop.table(lda_2, 1))
-
-
-
-
-
+# On regarde le taux de bon classé
+cbind(diag(prop.table(lda_, 1)), diag(prop.table(lda_moy, 1)) ,diag(prop.table(lda_reg, 1)))
